7daa0ec511d65f6231fcbabf54af5ad90b6d6e2a
Guilherme <guiartbp@gmail.com>
2023-02-19 21:12:33 -0300

    done 8_methods

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -9,25 +9,33 @@
 
 def ascii_translator(number)
     number.chr
-end
-
-
+end 
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
 
+def common_sports(current, favorite)
+    current & favorite
+end
 
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
 
+def alphabetical_list (games)
+    games.sort.uniq
+end    
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
+
+def lucky_number(number = 7)
+    "Today's lucky number is #{number}"
+end
 
 
 # method name: #ascii_code
@@ -36,6 +44,12 @@ end
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
 
+def ascii_code(string)
+    unless string.length == 1
+        return "Input Error"
+    end
+    string.ord
+end
 
 # method name: #pet_pun
 # parameter: animal (a string)
@@ -45,9 +59,20 @@ end
 # console output: otherwise, "I think <animal>s have pet-tential!" (potential)
 # hint: use puts
 
+def pet_pun(animal)
+    if animal == 'cat'
+        return puts 'Cats are purr-fect!'
+    elsif animal == 'dog'
+        return puts 'Dogs are paw-some!'
+    end
+    puts "I think #{animal}s have pet-tential!"
+end
 
 # method name: #twenty_first_century?
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
 
+def twenty_first_century? (year)
+    year.between?(2001, 2100)
+end
