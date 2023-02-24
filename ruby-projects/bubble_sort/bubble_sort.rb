884aa2515302b126a61052d50c0a89f08e1b7048
Guilherme <guiartbp@gmail.com>
2023-02-24 14:54:23 -0300

    buble_sort done

---
+++ ruby-projects/bubble_sort/bubble_sort.rb
@@ -0,0 +1,21 @@
+sort = [4,3,78,2,0,2]
+
+def bubble_sort (numbers)
+
+    size_repetition = (numbers.length - 1)
+    
+    loop do
+        swapped = false
+        (1..size_repetition).each do |i|
+            if numbers[i-1] > numbers[i]
+                numbers[i-1], numbers[i] = numbers[i], numbers[i-1]
+                swapped = true
+            end
+        end
+        size_repetition -= 1
+        break if !swapped 
+    end
+    print numbers
+end
+
+bubble_sort(sort)
