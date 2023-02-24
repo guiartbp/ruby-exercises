3b494f985fc91c40bdd3504e18e2c639f922a0bc
Guilherme <guiartbp@gmail.com>
2023-02-24 10:39:51 -0300

    stock_picker.rb done

---
+++ ruby-projects/stock_picker/stock_picker.rb
@@ -0,0 +1,24 @@
+prices = [17,3,6,9,15,8,6,1,10]
+
+def stock_picker (prices)
+    
+    c = 0
+    adress = []
+    prices.each_with_index do |num, idx| 
+        for i in 0..(prices.length - 1)
+            if i == idx
+                puts i
+            elsif i < idx
+                if num - prices[i] > c
+                    c = num - prices[i]
+                    adress = [i, idx]
+                end
+            end
+        end
+    end
+    print adress
+
+                
+end
+
+stock_picker(prices)
